2c6e8c0f3901bd0cc6ca4b6e4266a6e99634d132
Valdum <56064897+V4ldum@users.noreply.github.com>
2025-05-14 15:59:25 +0200

    Update lock.sh

--- api/scripts/lock.sh
+++ api/scripts/lock.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 LOCK_FILE="/tmp/build.lock"
 HOME="/root/finance"
 flock -n $LOCK_FILE -c "bash $HOME/finance-back/api/scripts/deploy_if_changed.sh" >> "$HOME/logs/finance-deploy.log" 2>&1
